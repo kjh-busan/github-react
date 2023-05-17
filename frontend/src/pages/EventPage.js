ed589b02ce617a251d6cdac9152a065eec97c6a1
kjh-busan <picsmash7@gmail.com>
2023-05-17 22:41:29 +0900

    feat: add eventsPage

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "another event",
+  },
+];
 export default function EventPage() {
   return <div>EventPage</div>;
 }
