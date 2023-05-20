12778f08666443328d875952a32b7a454de1aa77
kjh-busan <picsmash7@gmail.com>
2023-05-20 09:34:30 +0900

    feat: mod eventpage.js

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,30 +1,8 @@
-import { useEffect, useState } from "react";
-
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
-  return (
-    <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-    </>
-  );
+  return <EventsList events={fetchedEvents} />;
 }
 
 export default EventsPage;
