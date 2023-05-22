9a2e44d223c84e431519818b12e6c5e728df0e63
kjh-busan <picsmash7@gmail.com>
2023-05-23 05:45:23 +0900

    feat: root.js

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,7 +1,10 @@
+import { Outlet, useLoaderData } from "react-route-dom";
 import MainNavigation from "./components/MainNavigation";
-import { Outlet } from "react-route-dom";
 
 export default function RootLayout() {
+  const events = useLoaderData();
+  console.log(events);
+
   return (
     <>
       <MainNavigation />
