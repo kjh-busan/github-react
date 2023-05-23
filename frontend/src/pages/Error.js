bd7b95a11e3f1a7c8152fb6a362a091239e3916d
kjh-busan <picsmash7@gmail.com>
2023-05-24 00:00:11 +0900

    feat: useRouteError

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,7 @@
+import { useRouteError } from "react-router-dom";
 import React from "react";
 
 export default function ErrorPage() {
-  return <div>An error occurered!</div>;
+  const error = useRouteError();
+  return <div>An error occurered! </div>;
 }
