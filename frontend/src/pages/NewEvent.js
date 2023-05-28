9acd5901718bbc9f2d42c7119bd8d1f6de356e2a
kjh-busan <picsmash7@gmail.com>
2023-05-29 00:06:43 +0900

    feat: app.js action to json

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,4 +1,4 @@
-import { json } from "react-router-dom";
+import { redirect, json } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
@@ -30,4 +30,6 @@ export async function action({ request, params }) {
   if (!response.ok) {
     throw json({ message: "Could not find event" }, { status: 500 });
   }
+
+  return redirect("/events");
 }
