fa978884965c70f196236234abec93b492f43f66
kjh-busan <picsmash7@gmail.com>
2023-05-30 22:25:06 +0900

    feat: NewEvent response return

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -27,6 +27,10 @@ export async function action({ request, params }) {
     body: JSON.stringify(eventData),
   });
 
+if (response.status === 422) {
+  return response;
+}
+
   if (!response.ok) {
     throw json({ message: "Could not find event" }, { status: 500 });
   }
