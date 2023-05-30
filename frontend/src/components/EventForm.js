ca626afbf7b5674197919826dad1e89a2a25d57e
kjh-busan <picsmash7@gmail.com>
2023-05-30 22:18:03 +0900

    feat: useNavigation

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,9 +1,13 @@
-import { Form, useNavigate } from "react-router-dom";
+import { Form, useNavigate, useNavigation } from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === "submitting";
+
   function cancelHandler() {
     navigate("..");
   }
