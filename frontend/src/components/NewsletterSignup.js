074d6f997cd40f14469554496eb9baf3adb4dbf5
kjh-busan <picsmash7@gmail.com>
2023-05-31 21:45:35 +0900

    fix: Form

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,18 @@
+import { useFetcher, Form } from "react-router-dom";
 import classes from "./NewsletterSignup.module.css";
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <Form method="post" className={classes.newsletter}>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </Form>
   );
 }
 
