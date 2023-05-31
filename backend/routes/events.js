5e96c812eb44d7f8624d115875f28d316cda918a
kjh-busan <picsmash7@gmail.com>
2023-05-31 21:50:56 +0900

    feat: useFetcher router delay

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
     const events = await getAll();
     setTimeout(() => {
       res.json({ events: events });
-    }, 1500);
+    }, 2000);
   } catch (error) {
     next(error);
   }
