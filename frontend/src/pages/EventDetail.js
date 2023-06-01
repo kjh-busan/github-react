cff4dceb25260a526a41a8aabeed8977f9ebf087
kjh-busan <picsmash7@gmail.com>
2023-06-01 22:23:33 +0900

    feat: EventDetail.js async loadEvent function

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -4,29 +4,35 @@ import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
   const data = useRouteLoaderData("event-detail");
-  return <EventItem EventItem event={data.event} />;
+  return (
+    <>
+      <EventItem EventItem event={data.event} />
+      <EventsList events={data.events} />
+    </>
+  );
 }
 
 export default EventDetailPage;
 
-export async function load({request, params}) {
+async function loadEvent(id) {
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json({ messsage: "Could not fetch event details" }, { status: 500 });
+  } else {
+    return response;
+  }
+}
+export async function load({ request, params }) {
   const id = params.eventId;
 
- const response = await fetch("http://localhost:8080/e vents/" + id, {
-   method: request.method,
- });
-
- if (!response.ok) {
-  throw json({messsage: 'Could not fetch event details'}, {status: 500})
-
- } else {
-  return response;
- }
+  const response = await fetch("http://localhost:8080/e vents/" + id, {
+    method: request.method,
+  });
 }
 
 export async function action() {
   const eventId = params.eventId;
-  const response = await fetch("http://localhost:8080/events/" + eventId);
 
   if (!response.ok) {
     throw json({ messsage: "Could not delete event" }, { status: 500 });
