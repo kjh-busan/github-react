5c0126c66f63ad42d82757fb86f727880f43bc34
kjh-busan <picsmash7@gmail.com>
2023-06-02 07:49:32 +0900

    fix: modules

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,36 +1,37 @@
-import { Suspense } from "react";
-import { useLoaderData, defer, json, Await } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const { events } = useLoaderData();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
-    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
-      <Await resolve={events}>
-        {(loaderEvents) => <EventsList events={loaderEvents}></EventsList>}
-      </Await>
-    </Suspense>
+    <>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
   );
 }
+
 export default EventsPage;
-
-async function loaderEvents() {
-  // setIsLoading(true);
-  const response = await fetch("http://localhost:8080/events");
-
-  if (!response.ok) {
-    throw json({ message: "Fetching events failed." }, { status: 500 });
-  }
-   else {
-    const resData = await response.json();
-    return resData.events;
-  }
-  // setIsLoading(false);
-  // fetchEvents();
-}
-
-export function loader() {
-  defer({
-    event: loadEvents(),
-  });
-}
